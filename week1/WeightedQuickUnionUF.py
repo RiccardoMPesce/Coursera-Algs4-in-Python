a357f6d219cb98647d3870d4638874bb5c42aa34
riccardompesce <riccardompesce@gmail.com>
2021-11-17 20:10:16 +0100

    Implemented find

--- week1/WeightedQuickUnionUF.py
+++ week1/WeightedQuickUnionUF.py
@@ -4,6 +4,16 @@ class WeightedQuickUnionUF:
         self._parent = [i for i in range(n)]
         self._size = [1] * n
 
-    @property
     def count(self):
         return self._count
+
+    def validate(self, p):
+        n = len(self._parent)
+        if p < 0 or p > n - 1:
+            raise IndexError(f"index is not between {0} and {n - 1}")
+
+    def find(self, p):
+        self.validate(p)
+        while (p != self._parent[p]):
+            p = self._parent[p]
+        return p
