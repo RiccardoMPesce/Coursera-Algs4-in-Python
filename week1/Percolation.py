b51f4a314b7a72f7a011b95efda463ff449e4559
riccardompesce <riccardompesce@gmail.com>
2021-11-17 21:36:06 +0100

    Percolation

--- week1/Percolation.py
+++ week1/Percolation.py
@@ -1,2 +1,22 @@
+from . import WeightedQuickUnionUF
+
 class Percolation:
-    pass
+    def __init__(self, n):
+        self.n = n
+        self.grid = WeightedQuickUnionUF.WeightedQuickUnionUF(n * n)
+
+    def open(self, row, col):
+        pass
+    
+    def is_open(self, row, col):
+        return self.grid[row - 1][col - 1] == 0
+
+    def is_full(self, row, col):
+        return self.grid[row - 1][col - 1] == 1
+
+    def number_of_open_sites(self):
+        return sum([sum([int(cell == 0) for cell in r]) for r in self.grid])
+
+    def percolates(self):
+        pass 
+
